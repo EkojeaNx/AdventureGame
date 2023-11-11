f50ec601660c05d6f4b4b2eb75b464705f5d3638
Türkeş Ekrem Şentürk <turkesekremsenturk@outlook.com>
2023-11-12 02:23:52 +0300

    Adventure Game (Version: 0.0.0.2) - Player eklendi

--- src/main/java/Core/Game.java
+++ src/main/java/Core/Game.java
@@ -1,5 +1,6 @@
 package Core;
 
+import Players.Player;
 import Settings.ColorSettings;
 import Settings.GameSettings;
 
@@ -14,6 +15,35 @@ public class Game {
 
         // Oyuncu rehberi
 		about();
+
+        // Oyuncu Oluşturma
+        String playerName;
+        do {
+            GameSettings.input("Oyuncu Adını Giriniz: ");
+			playerName = GameSettings.inputScanner.nextLine();
+			System.out.print(ColorSettings.RESET);
+
+			playerName = playerName.toString().trim();
+
+			if (!playerName.isEmpty()) {
+				if (playerName.length() > 5) {
+					break;
+				} else {
+					GameSettings.warningMessage("Oyuncu adı 6 haneden az olamaz!");
+				}
+			} else {
+				GameSettings.warningMessage("Oyuncu adı boş bırakılamaz!");
+			}
+        } while (true);
+
+        Player gamePlayer = new Player(playerName);
+
+        System.out.println();
+		GameSettings.align(20);
+		System.out.println(ColorSettings.WHITE_BOLD + "Bu puslu ve macera dolu oyuna Hoş Geldin " + ColorSettings.RESET
+				+ ColorSettings.CYAN_BOLD_BRIGHT + gamePlayer.getPlayerName() + ColorSettings.RESET);
+
+		System.out.println();
     }
 
     // Oyuncu rehberi
@@ -24,6 +54,13 @@ public class Game {
 		System.out.println(ColorSettings.YELLOW_UNDERLINED + "### Oyun Rehberi ###" + ColorSettings.RESET);
 
 		// Rehber
+		System.out.println();
+
+        GameSettings.align(20);
+		GameSettings.article("Oyuna başladığınızda oyuncu ismi giriyorsunuz.");
+		GameSettings.align(20);
+		GameSettings.article("Oyuncu isminiz boş ve 6 haneden küçük olmamalı.");
+
 		System.out.println();
     }
 }
